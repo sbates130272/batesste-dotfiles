a0e7d412822312452427125fbcae90cf15e2f7b5
Stephen Bates <sbates@raithlin.com>
2026-09-08 11:23:29 -0600

    fix(emacs): consolidate init into init.el, drop .emacs
    When both ~/.emacs and ~/.emacs.d/init.el exist Emacs loads only
    ~/.emacs and ignores init.el entirely. The package bootstrap in
    init.el never ran, rust-mode was never installed, and the
    (require 'rust-mode) in .emacs errored on every startup.
    
    Merge everything into .emacs.d/init.el and delete .emacs. Guard
    package-refresh-contents behind a (package-installed-p) check so
    the network hit only occurs when rust-mode is actually missing.
    
    Signed-off-by: Stephen Bates <sbates@raithlin.com>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,20 @@
 (require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
-(package-refresh-contents)
+
+(unless (package-installed-p 'rust-mode)
+  (package-refresh-contents)
+  (package-install 'rust-mode))
+
+(global-font-lock-mode 1)
+(line-number-mode 1)
+(column-number-mode 1)
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 4
+      tab-width 4)
+(setq-default show-trailing-whitespace t)
+(setq-default auto-fill-function 'do-auto-fill)
+
+(require 'rust-mode)
+(add-to-list 'auto-mode-alist '("\\.hip\\'" . c++-mode))
